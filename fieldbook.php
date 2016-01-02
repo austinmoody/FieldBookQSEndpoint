b75c1d17d6bde9c9ea19c7ec4e702abb7716989c
Austin Moody <amoody@mail.notaustintexas.com>
2016-01-02 14:22:11 -0500

    Renamed php file.  Now reads proper username and api from files that will need to be on the filesystem.

--- fieldbook.php
+++ fieldbook.php
@@ -1,9 +1,20 @@
 <?php
+
+	define("__USERFILE__","/home/amoody/.fieldbook_username");
+	define("__APIFILE__","/home/amoody/.fieldbook_api");
         
-	$possible_actions = array("mood");
-    	$user_name = "austinqs";
-    	$api_key = "xxxyyy";
-    
+	$possible_actions = array("coffee");
+
+	// Read correct user_name from file
+	$file_user_name = fopen(__USERFILE__,"r") or die("Unable to open username file.");
+	$user_name = rtrim(fread($file_user_name,filesize(__USERFILE__)));
+	fclose($file_user_name);
+
+	// Read correct api key from file
+	$file_api = fopen(__APIFILE__,"r") or die("Uble to open api file.");
+	$api_key = rtrim(fread($file_api,filesize(__APIFILE__)));
+	fclose($file_api);
+
 	$return_value = array(array("Status" => "FAIL","Response" => ""));
     
     if ( isset($_GET["action"]) && in_array($_GET["action"],$possible_actions) ) 
@@ -16,6 +27,7 @@
 	}
 	else {
 		$return_value[0]["Response"] = "INVALID CREDENTIALS";
+		$return_value[0]["Status"] = $user_name;
 	}
     } 
     else 
