3525f67b7cd20d5af4cf6c59fc5f56c732a74f65
austinmoody <austin.moody@gmail.com>
2016-01-03 05:05:59 +0000

    Adding coffee should be working now.  Added example settings file to repo.

--- fieldbook.php
+++ fieldbook.php
@@ -2,11 +2,12 @@
 
 	require __DIR__ . '/vendor/autoload.php';
 
-	define("__SETTINGSFILE__","/home/amoody/.fieldbook.json");
-        
+    // TODO - need to come up with a standard place to store this file.
+	define("__SETTINGSFILE__","/mnt/user/.fieldbook.json");
+
 	$possible_actions = array("coffee");
 
-	// Read settings
+	// Read settings file
 	$settings_file = fopen(__SETTINGSFILE__,"r") or die ("Unable to open settings file.");
 	$fieldbook_settings = json_decode(fread($settings_file,filesize(__SETTINGSFILE__)),true);
 	fclose($settings_file);
@@ -15,36 +16,42 @@
 	$api_key = $fieldbook_settings["api"];
 	$fieldbook_username = $fieldbook_settings["fieldbook_username"];
 	$fieldbook_key = $fieldbook_settings["fieldbook_key"];
+    $baseUrl = $fieldbook_settings["base_url"];
+
+    $currentFullDate = date('m/d/Y h:i:s a', time());
+    $currentShortDate = date('m/d/Y', time());
 
 	$return_value = array(array("Status" => "FAIL","Response" => ""));
-    
-    if ( isset($_GET["action"]) && in_array($_GET["action"],$possible_actions) ) 
+
+    if ( isset($_GET["action"]) && in_array($_GET["action"],$possible_actions) )
     {
-	// Action is good, now check user and api_key
+	    // Action is good, now check user and api_key
 
-	if ( (isset($_GET["user"]) && $_GET["user"] == $user_name) && (isset($_GET["api"]) && $_GET["api"] == $api_key) ) {
-		// User & API good - get down to other business
-		$return_value[0]["DEBUG"] = "Username Good";
-
-		// Httpful testing
-		$url = "https://api.fieldbook.com/v1/5681f3c36b1eca03000a12c2/coffee";
-
-		$response = \Httpful\Request::get($url)
-				->expectsJson()
-				->authenticateWith($fieldbook_username,$fieldbook_key)
-				->send();
-		echo $response;
-
-	}
-	else {
-		$return_value[0]["Response"] = "INVALID CREDENTIALS";
-	}
-    } 
-    else 
+	    if ( (isset($_GET["user"]) && $_GET["user"] == $user_name) && (isset($_GET["api"]) && $_GET["api"] == $api_key) ) {
+		    // User & API good - get down to other business
+            switch (strtoupper($_GET["action"])) {
+                case "COFFEE":
+                    $jsonBody = '{"count":1,"datetime":"' . $currentFullDate . '","date":"' . $currentShortDate . '"}';
+                    $coffeeUrl = $baseUrl . "coffee";
+                    $response = \Httpful\Request::post($coffeeUrl)
+            		            ->sendsJson()
+            		            ->authenticateWith($fieldbook_username,$fieldbook_key)
+                                ->body($jsonBody)
+            		            ->send();
+                    $return_value[0]["Status"] = "Success";
+                    $return_value[0]["Response"] = "Coffee added";
+                    break;
+            }
+	    }
+	    else {
+		    $return_value[0]["Response"] = "INVALID CREDENTIALS";
+	    }
+    }
+    else
     {
        $return_value[0]["Response"] = "INVALID ACTION";
     }
-    
-   //echo json_encode($return_value);
+
+   echo json_encode($return_value);
 
 ?>
